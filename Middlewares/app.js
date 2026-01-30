48a850579ad9e8ea74134b98c16801fffc074f51
Sitaram8472 <sitaramsahu8472@gmail.com>
2026-01-31 02:58:42 +0530

    update in middlewares

--- Middlewares/app.js
+++ Middlewares/app.js
@@ -17,14 +17,40 @@ const app = express();
 //   console.log("i am after middleware");
 // });
 
-
 // utlitlty logger
-app.use((req, res, next) => {
-  req.time = Date.now;
-  console.log(req.method, req.hostname, req.path, req.time);
-  next();
+// app.use((req, res, next) => {
+//   req.time = Date.now;
+//   console.log(req.method, req.hostname, req.path, req.time);
+//   next();
+// });
+
+// api token
+// app.use("/api", (req, res, next) => {
+//   let { token } = req.query;
+//   if (token === "giveaccess") {
+//     next();
+//   }
+//   res.send("access denied");
+// });
+
+// app.get("/api", (req, res) => {
+//   res.send("i am giev ypu access");
+// });
+
+// series of middlewares
+let checktoken = (req, res, next) => {
+  let { token } = req.query;
+  if (token === "giveaccess") {
+    next();
+  }
+  res.send("access denied");
+};
+
+app.get("/api", checktoken, (req, res) => {
+  res.send("i am giev ypu access");
 });
 
+// universe
 
 app.get("/", (req, res) => {
   res.send("i am root");
@@ -34,10 +60,10 @@ app.get("/random", (req, res) => {
   res.send("i am random");
 });
 
-
-
-
-
+// not found page using middleware
+app.use((req, res) => {
+  res.status(404).send("Page not found!");
+});
 
 // universal
 app.listen(8080, () => {
